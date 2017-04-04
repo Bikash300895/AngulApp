007500f4d5d831d10c0d57eff56759b35887c1ce
Bikash <bikash11roy@gmail.com>
2017-04-04 06:10:40 +0600

    Hero seperated and detail page sepereaed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {HeroDetailComponent} from "./Components/hero-detail.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeroDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +16,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeroDetailComponent]
 })
 export class AppModule { }
