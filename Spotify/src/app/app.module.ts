8d36accb9cd12f80260af1efc0fc0e8d94eccca3
Bikash <bikash11roy@gmail.com>
2017-04-09 02:51:17 +0600

    Router added

--- Spotify/src/app/app.module.ts
+++ Spotify/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {HomeComponent} from "./components/Home/home.component";
 import {NavbarComponent} from "./components/navbar/navbar.component";
 import {AboutComponent} from "./components/about/about.component";
 import {SearchComponent} from "./components/search/search.component";
+import {routing} from "./app.routing";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import {SearchComponent} from "./components/search/search.component";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
