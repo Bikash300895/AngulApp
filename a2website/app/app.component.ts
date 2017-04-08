5f60783afd4c36f24b6d5c290a2e39af0c787ffa
Bikash <bikash11roy@gmail.com>
2017-04-08 06:28:21 +0600

    Creating custom component

--- a2website/app/app.component.ts
+++ a2website/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Your first Angular 2 app</h1>'
+  template: '<h1>Your first Angular 2 app</h1> <navbar></navbar>'
 })
 export class AppComponent { }
