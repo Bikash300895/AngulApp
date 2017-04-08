97206cc1b88b5621a230d33efeced5f873c16810
Bikash <bikash11roy@gmail.com>
2017-04-09 02:34:20 +0600

    Search Component created

--- Spotify/src/app/app.module.ts
+++ Spotify/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppComponent } from './app.component';
 import {HomeComponent} from "./components/Home/home.component";
 import {NavbarComponent} from "./components/navbar/navbar.component";
 import {AboutComponent} from "./components/about/about.component";
+import {SearchComponent} from "./components/search/search.component";
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, AboutComponent, NavbarComponent
+    AppComponent, HomeComponent, AboutComponent, NavbarComponent, SearchComponent
   ],
   imports: [
     BrowserModule,
