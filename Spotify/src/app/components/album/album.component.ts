738e93e9456dbc29e35acc0bb0a6f3d817a98994
Bikash <bikash11roy@gmail.com>
2017-04-09 06:55:53 +0600

    Data for album got and tested

--- Spotify/src/app/components/album/album.component.ts
+++ Spotify/src/app/components/album/album.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {SpotifyService} from "../../services/spotify.service";
 
 @Component({
   selector: 'album',
-  templateUrl: './album.component.html'
+  templateUrl: './album.component.html',
+  providers: [SpotifyService]
 })
-export class AlbumComponent {
+export class AlbumComponent implements OnInit{
+  ngOnInit(): void {
+    this._route.params
+      .map(params => params['id'])
+      .subscribe( (id) => {
+        this._spotifyService.getAlbum(id)
+          .subscribe(album => {
+            this.album = album;
+          });
+      });
+  }
+  id: string;
+  artist: any;
+  album: any;
 
+  constructor(private _spotifyService: SpotifyService,
+              private _route: ActivatedRoute)
+  {
+
+  }
 }
