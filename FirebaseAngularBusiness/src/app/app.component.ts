048ef5fbafb586e0ea1257ec9a4eb0d685376278
Bikash <bikash11roy@gmail.com>
2017-04-10 07:26:56 +0600

    Using firebase service to get data

--- FirebaseAngularBusiness/src/app/app.component.ts
+++ FirebaseAngularBusiness/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FirebaseService} from "./services/firebase.services";
 
 
@@ -8,7 +8,23 @@ import {FirebaseService} from "./services/firebase.services";
   styleUrls: ['./app.component.css'],
   providers: [FirebaseService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  business: any;
+  categories: any;
+
+  constructor(private _firebaseService: FirebaseService){
+
+  }
+
+  ngOnInit(): void {
+    this._firebaseService.getBusiness().subscribe(business => {
+      this.business = business;
+    });
+
+    this._firebaseService.getCategories().subscribe(categories => {
+      this.categories = categories;
+    });
+
+  }
 
 }
