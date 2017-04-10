b424e8a50c19bed23cd7c03e60d4732490d1aaff
Bikash <bikash11roy@gmail.com>
2017-04-10 23:44:05 +0600

    Business model created

---
+++ FirebaseAngularBusiness/src/app/Model/business.model.ts
@@ -0,0 +1,10 @@
+export class Business{
+  category: string;
+  city: string;
+  company: string;
+  description: string;
+  email: string;
+  phone: string;
+  state: string;
+  zipcode: string;
+}
