dae3372b340d31f0f80de15cfa740e20c3a07a7b
Bikash <bikash11roy@gmail.com>
2017-04-11 21:41:50 +0600

    page router dependency added

--- AuthO/src/app/app.module.ts
+++ AuthO/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {routing,appRoutingProviders} from './app.routing';
 
 import { AppComponent } from './app.component';
 import {ProfileComponent} from "./component/profile/profile.component";
@@ -15,8 +16,9 @@ import {HomeComponent} from "./component/home/home.component";
     BrowserModule,
     FormsModule,
     HttpModule,
+    routing,
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
