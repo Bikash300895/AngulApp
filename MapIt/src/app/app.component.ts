6848d4411aa745c3c32578988f6edf4b86e62944
Bikash <bikash11roy@gmail.com>
2017-04-12 22:33:01 +0600

    Map populated with marker data

--- MapIt/src/app/app.component.ts
+++ MapIt/src/app/app.component.ts
@@ -6,7 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Map it';
+  zoom: number = 10;
+  // Start position
   lat: number = 51.678418;
   lng: number = 7.809007;
+
+  markers: marker[] = [
+    {
+      name: 'company one',
+      lat:  51.678418,
+      lng: 7.809007,
+      draggable: true
+    },
+
+    {
+      name: 'company two',
+      lat:  51.678418,
+      lng: 9.809007,
+      draggable: true
+    },
+
+    {
+      name: 'company three',
+      lat:  51.678418,
+      lng: 8.809007,
+      draggable: true
+    },
+
+    {
+      name: 'company four',
+      lat:  53.678418,
+      lng: 7.809007,
+      draggable: true
+    },
+
+  ]
+
+  constructor(){
+
+  }
+}
+
+interface marker{
+  name?: string;
+  lat: number;
+  lng: number,
+  draggable: boolean
 }
